134611698ec0744cf03d09277da394f7648d1ce3
Ingmar Steiner <steiner@coli.uni-saarland.de>
2013-11-05 19:08:47 +0100

    resolve, retrieve, install working IFF file actually exists remotely

--- src/main/java/marytts/tools/newinstall/Installer.java
+++ src/main/java/marytts/tools/newinstall/Installer.java
@@ -1,10 +1,13 @@
 package marytts.tools.newinstall;
 
 import java.io.IOException;
+import java.net.URL;
 import java.text.ParseException;
 
 import org.apache.ivy.Ivy;
+import org.apache.ivy.core.install.InstallOptions;
 import org.apache.ivy.core.module.descriptor.ModuleDescriptor;
+import org.apache.ivy.core.resolve.ResolveOptions;
 import org.apache.ivy.core.retrieve.RetrieveOptions;
 import org.apache.ivy.core.settings.IvySettings;
 import org.apache.ivy.plugins.parser.xml.XmlModuleDescriptorParser;
@@ -29,17 +32,26 @@ public class Installer {
 		ivySettings.load(Resources.getResource("ivysettings.xml"));
 
 		// as a test, parse module descriptor for lang-de component
-		ModuleDescriptor descriptor = XmlModuleDescriptorParser.getInstance().parseDescriptor(ivySettings,
-				Resources.getResource("marytts-lang-de/ivy-5.1-SNAPSHOT.xml"), true);
+		URL resource = Resources.getResource("marytts-voice-cmu-slt-hsmm-5.0-SNAPSHOT.xml");
+		ModuleDescriptor descriptor = XmlModuleDescriptorParser.getInstance().parseDescriptor(ivySettings, resource, true);
 
 		// instantiate ivy
 		Ivy ivy = Ivy.newInstance(ivySettings);
 		// set ivy loglevel to DEBUG
 		ivy.getLoggerEngine().pushLogger(new DefaultMessageLogger(Message.MSG_DEBUG));
+		ResolveOptions resolveOptions = new ResolveOptions();
+		ivy.resolve(descriptor, resolveOptions);
 		// retrieve options; retrieved artifacts go into MARYBASE/lib
-		RetrieveOptions retrieveOptions = new RetrieveOptions()
-				.setDestArtifactPattern("${mary.base}/lib/[artifact]-[revision].[ext]");
+		RetrieveOptions retrieveOptions = new RetrieveOptions();
+		retrieveOptions.setDestIvyPattern("${mary.base}/installed/[module]-[revision].xml");
+		retrieveOptions.setDestArtifactPattern("${mary.base}/lib/[module]-[revision].[ext]");
 		ivy.retrieve(descriptor.getModuleRevisionId(), retrieveOptions);
-	}
 
+		InstallOptions installOptions = new InstallOptions();
+		installOptions.setTransitive(true);
+		installOptions.setOverwrite(true);
+		String from = "remote";
+		String to = "marytts-installed";
+		ivy.install(descriptor.getModuleRevisionId(), from, to, installOptions);
+	}
 }
