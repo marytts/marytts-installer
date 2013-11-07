66c440d3fc1f9cd59da489b065760b7a3de58a1b
Jonathan Poitz <jonathanpoitz@gmail.com>
2013-11-07 16:58:04 +0100

    implemented Component object representing modules and InstallerCLI for calling relevant methods on the command line

--- src/main/java/marytts/tools/newinstall/Installer.java
+++ src/main/java/marytts/tools/newinstall/Installer.java
@@ -1,8 +1,14 @@
 package marytts.tools.newinstall;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import marytts.tools.newinstall.objects.Component;
 
 import org.apache.ivy.Ivy;
 import org.apache.ivy.core.install.InstallOptions;
@@ -18,40 +24,117 @@ import com.google.common.io.Resources;
 
 public class Installer {
 
-	/**
-	 * Test Installer <br>
-	 * <b>Note:</b> must currently run with -Dmary.base=/path/to/marytts
-	 * 
-	 * @param args
-	 * @throws IOException
-	 * @throws ParseException
-	 */
-	public static void main(String[] args) throws ParseException, IOException {
-		// load ivy settings
-		IvySettings ivySettings = new IvySettings();
-		ivySettings.load(Resources.getResource("ivysettings.xml"));
+  private List<Component> resources;
+  private IvySettings ivySettings;
 
-		// as a test, parse module descriptor for lang-de component
-		URL resource = Resources.getResource("marytts-voice-cmu-slt-hsmm-5.0-SNAPSHOT.xml");
-		ModuleDescriptor descriptor = XmlModuleDescriptorParser.getInstance().parseDescriptor(ivySettings, resource, true);
 
-		// instantiate ivy
-		Ivy ivy = Ivy.newInstance(ivySettings);
-		// set ivy loglevel to DEBUG
-		ivy.getLoggerEngine().pushLogger(new DefaultMessageLogger(Message.MSG_DEBUG));
-		ResolveOptions resolveOptions = new ResolveOptions();
-		ivy.resolve(descriptor, resolveOptions);
-		// retrieve options; retrieved artifacts go into MARYBASE/lib
-		RetrieveOptions retrieveOptions = new RetrieveOptions();
-		retrieveOptions.setDestIvyPattern("${mary.base}/installed/[module]-[revision].xml");
-		retrieveOptions.setDestArtifactPattern("${mary.base}/lib/[module]-[revision].[ext]");
-		ivy.retrieve(descriptor.getModuleRevisionId(), retrieveOptions);
+  public Installer() {
 
-		InstallOptions installOptions = new InstallOptions();
-		installOptions.setTransitive(true);
-		installOptions.setOverwrite(true);
-		String from = "remote";
-		String to = "marytts-installed";
-		ivy.install(descriptor.getModuleRevisionId(), from, to, installOptions);
-	}
+    try {
+      this.resources = new ArrayList<Component>();
+      this.ivySettings = new IvySettings();
+      this.ivySettings.load(Resources.getResource("ivysettings.xml"));
+      parseIvyResources(this.ivySettings);
+    } catch (ParseException e) {
+      e.printStackTrace();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+
+  /**
+   * @param ivySettings
+   * @throws ParseException
+   * @throws IOException
+   */
+  private void parseIvyResources(IvySettings ivySettings)
+      throws ParseException, IOException {
+
+    String resourcesPath =
+      "/Users/Jonathan/marytts/marytts-installer/src/main/resources";
+    File[] resourcesList = new File(resourcesPath).listFiles();
+
+    for (File oneFile : resourcesList) {
+      if (oneFile.getName().startsWith("marytts-voice")) {
+        URL oneResource = Resources.getResource(oneFile.getName());
+        ModuleDescriptor descriptor =
+          XmlModuleDescriptorParser.getInstance().parseDescriptor(
+            ivySettings, oneResource, true);
+        Component oneComponent = new Component(descriptor);
+        this.resources.add(oneComponent);
+      }
+      else {
+        continue;
+      }
+    }
+
+  }
+
+
+  /**
+   * @return
+   */
+  public List<Component> getAvailableVoices() {
+
+    return this.resources;
+  }
+
+
+  /**
+   * @param feature
+   * @param value
+   * @return
+   */
+  public Collection<Component> getAvailableVoices(String feature, String value) {
+
+    // TODO
+    return null;
+  }
+
+
+  /**
+   * Test Installer <br>
+   * <b>Note:</b> must currently run with -Dmary.base=/path/to/marytts
+   * 
+   * @param args
+   * @throws IOException
+   * @throws ParseException
+   */
+  public static void main(String[] args)
+      throws ParseException, IOException {
+
+    // load ivy settings
+    IvySettings ivySettings = new IvySettings();
+    ivySettings.load(Resources.getResource("ivysettings.xml"));
+
+    // as a test, parse module descriptor for lang-de component
+    URL resource =
+      Resources.getResource("marytts-voice-cmu-slt-hsmm-5.1-SNAPSHOT.xml");
+    ModuleDescriptor descriptor =
+      XmlModuleDescriptorParser.getInstance().parseDescriptor(ivySettings,
+        resource, true);
+
+    // instantiate ivy
+    Ivy ivy = Ivy.newInstance(ivySettings);
+    // set ivy loglevel to DEBUG
+    ivy.getLoggerEngine().pushLogger(
+      new DefaultMessageLogger(Message.MSG_DEBUG));
+    ResolveOptions resolveOptions = new ResolveOptions();
+    ivy.resolve(descriptor, resolveOptions);
+    // retrieve options; retrieved artifacts go into MARYBASE/lib
+    RetrieveOptions retrieveOptions = new RetrieveOptions();
+    retrieveOptions
+      .setDestIvyPattern("${mary.base}/installed/[module]-[revision].xml");
+    retrieveOptions
+      .setDestArtifactPattern("${mary.base}/lib/[module]-[revision].[ext]");
+    ivy.retrieve(descriptor.getModuleRevisionId(), retrieveOptions);
+
+    InstallOptions installOptions = new InstallOptions();
+    installOptions.setTransitive(true);
+    installOptions.setOverwrite(true);
+    String from = "remote";
+    String to = "marytts-installed";
+    ivy.install(descriptor.getModuleRevisionId(), from, to, installOptions);
+  }
 }
---
+++ src/main/java/marytts/tools/newinstall/objects/Component.java
@@ -0,0 +1,210 @@
+/**
+ * 
+ */
+package marytts.tools.newinstall.objects;
+
+import java.util.Locale;
+
+import org.apache.ivy.core.module.descriptor.ModuleDescriptor;
+
+import com.google.common.collect.ComparisonChain;
+
+/**
+ * Java object representing marytts voices (components)
+ * 
+ * @author Jonathan
+ * 
+ */
+public class Component implements Comparable<Component> {
+
+  private String name;
+  private Locale locale;
+  private String gender;
+  private String type;
+  private String version;
+  private String licenseName;
+  private String description;
+
+
+  /**
+   * 
+   */
+  public Component() {
+
+  }
+
+
+  public Component(ModuleDescriptor descriptor) {
+
+    setGender(descriptor.getExtraAttribute("gender"));
+    setDescription(descriptor.getDescription());
+    setLicenseName(descriptor.getLicenses()[0].getName());
+    setLocale(new Locale(descriptor.getExtraAttribute("locale")));
+    setType(descriptor.getExtraAttribute("type"));
+    setVersion(descriptor.getAttribute("revision"));
+    setName(descriptor.getExtraAttribute("name"));
+  }
+
+
+  // TODO nulls for setters
+
+  /**
+   * @param locale
+   *          the locale to set
+   */
+  private void setLocale(Locale locale) {
+
+    this.locale = locale;
+  }
+
+
+  /**
+   * @param gender
+   *          the gender to set
+   */
+  private void setGender(String gender) {
+
+    this.gender = gender;
+  }
+
+
+  /**
+   * @param type
+   *          the type to set
+   */
+  private void setType(String type) {
+
+    this.type = type;
+  }
+
+
+  /**
+   * @param version
+   *          the version to set
+   */
+  private void setVersion(String version) {
+
+    this.version = version;
+  }
+
+
+  /**
+   * @param licenseName
+   *          the licenseName to set
+   */
+  private void setLicenseName(String licenseName) {
+
+    this.licenseName = licenseName;
+  }
+
+
+  /**
+   * @param description
+   *          the description to set
+   */
+  private void setDescription(String description) {
+
+    this.description = description;
+  }
+
+
+  /**
+   * @param name
+   *          the name to set
+   */
+  private void setName(String name) {
+
+    this.name = name;
+  }
+
+
+  /*
+   * (non-Javadoc)
+   * 
+   * @see java.lang.Object#toString()
+   */
+  @Override
+  public String toString() {
+
+    return "Component [locale=" + this.locale + ", gender=" + this.gender
+      + ", type=" + this.type + ", version=" + this.version + ", licenseName="
+      + this.licenseName + ", description=" + this.description + "]";
+  }
+
+
+  /**
+   * @return the name
+   */
+  public String getName() {
+
+    return this.name;
+  }
+
+
+  /**
+   * @return the locale
+   */
+  public Locale getLocale() {
+
+    return this.locale;
+  }
+
+
+  /**
+   * @return the gender
+   */
+  public String getGender() {
+
+    return this.gender;
+  }
+
+
+  /**
+   * @return the type
+   */
+  public String getType() {
+
+    return this.type;
+  }
+
+
+  /**
+   * @return the version
+   */
+  public String getVersion() {
+
+    return this.version;
+  }
+
+
+  /**
+   * @return the licenseName
+   */
+  public String getLicenseName() {
+
+    return this.licenseName;
+  }
+
+
+  /**
+   * @return the description
+   */
+  public String getDescription() {
+
+    return this.description;
+  }
+
+
+  @Override
+  public int compareTo(Component o) {
+
+    return ComparisonChain.start()
+      .compare(this.locale.toString(), o.getLocale().toString())
+      .compare(this.gender, o.getGender())
+      .compare(this.name, o.getName()).result();
+  }
+
+  // TODO getter/setter?
+  // TODO compareTo/equals...?
+
+}
