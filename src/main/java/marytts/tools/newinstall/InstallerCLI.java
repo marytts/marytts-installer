08d1672f3eb1b52fd479d73b7f6e4a96f937a5b1
Jonathan Poitz <jonathanpoitz@gmail.com>
2013-11-08 17:31:42 +0100

    add options to CLI

--- src/main/java/marytts/tools/newinstall/InstallerCLI.java
+++ src/main/java/marytts/tools/newinstall/InstallerCLI.java
@@ -11,6 +11,8 @@ import marytts.tools.newinstall.objects.Component;
 import org.apache.commons.cli.BasicParser;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.OptionBuilder;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 
@@ -34,25 +36,83 @@ public class InstallerCLI {
 
 		// create the Options
 		Options options = new Options();
-		options.addOption("l", "list", false, "list all available voice components");
-
+		// options.addOption("l", "list", false, "list all available voice components");
+		options.addOption("n", "name", true, "filter by name (also substrings possible");
+		options.addOption("l", "locale", true, "filter by locale");
+		options.addOption("g", "gender", true, "filter by gender");
+		options.addOption("t", "type", true, "filter by type");
+		options.addOption("i", "install", true, "install <name> component");
+		options.addOption("y", "yes", true, "always assume yes as an answer to prompts");
+		options.addOption(OptionBuilder.withLongOpt("gui").withDescription("starts GUI").create());
 		try {
-			// parse the command line arguments
-			CommandLine line = parser.parse(options, args);
+			List<Component> resources = installer.getAvailableVoices();
+			
+			parseCommandLine(args, parser, options, resources);
 
-			if (line.hasOption("list")) {
-				// --list: list all components
-				List<Component> resources = installer.getAvailableVoices();
-				listAllComponents(resources);
-
-			}
 		} catch (ParseException exp) {
 			System.out.println("Unexpected exception:" + exp.getMessage());
 		}
 
 	}
 
-	private static void listAllComponents(List<Component> resources) {
+	//TODO make unstatic
+	private static void parseCommandLine(String[] args, CommandLineParser parser, Options options, List<Component> resources)
+			throws ParseException {
+		// parse the command line arguments
+		CommandLine line = parser.parse(options, args);
+
+			// --list: list all components
+			if (line.hasOption("locale")) {
+				// --list --locale
+				String localeValue = line.getOptionValue("locale");
+				resources = filterResources(resources, "locale", localeValue);
+			}
+			if (line.hasOption("type")) {
+				// --list --type
+				String typeValue = line.getOptionValue("type");
+				resources = filterResources(resources, "type", typeValue);
+			}
+			if (line.hasOption("gender")) {
+				// --list --gender
+				String genderValue = line.getOptionValue("gender");
+				resources = filterResources(resources, "gender", genderValue);
+			}
+			if (line.hasOption("name")) {
+				// --list --name
+				String nameValue = line.getOptionValue("name");
+				resources = filterResources(resources, "name", nameValue);
+			}
+			printSortedComponents(resources);
+	}
+
+	private static List<Component> filterResources(List<Component> resources, String attribute, String attributeValue) {
+
+		for (Component oneComponent : resources) {
+			if (attribute.equals("locale")) {
+				if (!oneComponent.getLocale().toString().equalsIgnoreCase(attributeValue)) {
+					resources.remove(oneComponent);
+				}
+			} else if (attribute.equals("type")) {
+				if (!oneComponent.getType().equalsIgnoreCase(attributeValue)) {
+					resources.remove(oneComponent);
+				}
+			}
+			if (attribute.equals("gender")) {
+				if (!oneComponent.getGender().equalsIgnoreCase(attributeValue)) {
+					resources.remove(oneComponent);
+				}
+			}
+			if (attribute.equals("name")) {
+				if (!oneComponent.getName().equalsIgnoreCase(attributeValue)) {
+					resources.remove(oneComponent);
+				}
+			}
+		}
+
+		return resources;
+	}
+
+	private static void printSortedComponents(List<Component> resources) {
 
 		StringBuilder sb = new StringBuilder();
 		sb.append("listing all voice components:").append("\n\n");
